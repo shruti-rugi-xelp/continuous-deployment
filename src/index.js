0598b265bc8c3a67ff33e40fadf06f823a1e9211
shruti-rugi-xelp <shruti@xelpmoc.in>
2019-02-02 16:40:27 +0530

    modified files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const app = express()
     For the purpose of making the testing easier, we've just put this
     in a seperate file.
 */
+
 const myLib = require('./lib')
 
+const commitRef = process.env.APP_COMMIT_REF || 'N/A'
+const buildDate = process.env.APP_BUILD_DATE || new Date().toISOString()
 
-app.get('/', (req, res) => res.send(myLib.helloWorld()))
+
+app.get('/', (req, res) => {
+    const welcome = myLib.helloWorld()
+    const text = `${welcome}! We're at commit ${commitRef} which was built at ${buildDate}`
+    res.send(text)
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
