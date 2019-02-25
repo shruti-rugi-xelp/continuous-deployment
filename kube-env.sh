df28f59011469caddbf998b04956a236f977840e
shruti-rugi-xelp <shruti@xelpmoc.in>
2019-02-25 12:49:10 +0530

    Added setup files

---
+++ kube-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This needs to be run before any kubernetes comman
+#This also needs to be filled.
+
+export PROJECT_NAME="continuous-deployment"
+export PROJECT_ID="continuous-deployment-231910"
+export GOOGLE_COMPUTE_ZONE="us-central1-a"
+export GOOGLE_CLUSTER_NAME="standard-cluster"
+export CLOUD_PROVIDER="gcloud"
+export GOOGLE_CLUSTER_TYPE="zonal"
+export CLOUD_PROVIDER="gcloud"
---
+++ kube-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source "./kube-env.sh"
+if [ $CLOUD_PROVIDER == "gcloud" ]; then
+  if [ command -v gcloud >/dev/null 2>&1 ]; then
+    echo "gcloud not installed, please install using this link https://cloud.google.com/sdk/docs/#windows"
+    exit 1
+  else
+    echo $PROJECT_ID
+    gcloud config set project $PROJECT_ID
+
+    if [ $GOOGLE_CLUSTER_TYPE != "zonal" ] && [ $GOOGLE_CLUSTER_TYPE != "regional" ]; then
+      echo "This is not a valid cluster type, please enter zonal or regional"
+      exit 1
+    fi
+
+    if [ $GOOGLE_CLUSTER_TYPE == "zonal" ]; then
+      gcloud config set compute/zone $GOOGLE_COMPUTE_ZONE
+    else
+      gcloud config set compute/region $GOOGLE_COMPUTE_REGION
+    fi
+    gcloud container clusters get-credentials $GOOGLE_CLUSTER_NAME
+  fi
+  fi
